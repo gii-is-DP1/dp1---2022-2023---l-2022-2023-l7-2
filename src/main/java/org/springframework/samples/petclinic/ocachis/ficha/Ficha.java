8c75335c5828fb89d7387623eb16b3478d0a4179
Ernesto <ertorivero2661@gmail.com>
2022-10-21 16:12:57 +0200

    Entidades Color, Logro, Ficha y EstadisticasGlobales

--- src/main/java/org/springframework/samples/petclinic/ocachis/ficha/Ficha.java
+++ src/main/java/org/springframework/samples/petclinic/ocachis/ficha/Ficha.java
@@ -2,14 +2,11 @@ package org.springframework.samples.petclinic.ocachis.ficha;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
-import org.springframework.core.style.ToStringCreator;
 import org.springframework.samples.petclinic.model.BaseEntity;
-import org.springframework.samples.petclinic.ocachis.ficha.*;
+
 
 import lombok.Getter;
 
@@ -17,12 +14,8 @@ import lombok.Getter;
 
 @Entity
 @Table(name = "Ficha")
-public class Ficha extends Jugador {
+public class Ficha extends BaseEntity {
    
-	@ManyToOne
-	@JoinColumn(name = "jugador_id")
-	private Jugador jugador;
-
     @Column(name = "color")
 	@NotEmpty
 	private Color color;
--- src/main/java/org/springframework/samples/petclinic/ocachis/logro/Logro.java
+++ src/main/java/org/springframework/samples/petclinic/ocachis/logro/Logro.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,7 +17,7 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "Logro")
-public class Logro {
+public class Logro extends BaseEntity{
     @Column(name = "nombre")
 	@NotEmpty
 	private String nombre;
