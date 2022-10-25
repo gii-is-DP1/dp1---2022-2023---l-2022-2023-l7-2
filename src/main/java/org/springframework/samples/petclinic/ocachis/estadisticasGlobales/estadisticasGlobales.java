7a6a6f32bf7e3fc2d6b1d17892cacd6205c2e526
Ernesto <ernrivper@alum.us.es.email>
2022-10-25 10:58:19 +0200

    Cambio a EstadisticasGlobales

--- src/main/java/org/springframework/samples/petclinic/ocachis/estadisticasGlobales/estadisticasGlobales.java
+++ src/main/java/org/springframework/samples/petclinic/ocachis/estadisticasGlobales/estadisticasGlobales.java
@@ -9,59 +9,18 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.samples.petclinic.ocachis.Estadisticas;
+import org.springframework.samples.petclinic.ocachis.Jugador.Jugador;
+
 import lombok.Getter;
 
 @Getter
 @Entity
 @Table(name = "estadisticasGlobales")
 public class estadisticasGlobales {
-    @Column(name = "partidasJugadas")
+    @Column(name = "estadisticasGlobales")
 	@NotEmpty
-	private Integer parchisPartidasJugadas;
-
-	@Column(name = "promedioJugadores")
-	@NotEmpty
-	private Integer parchisPromedioJugadores;
-
-    @Column(name = "duracionMinima")
-	@NotEmpty
-	private Time parchisDuracionMinima;
-
-	@Column(name = "duracionMaxima")
-	@NotEmpty
-	private Time parchisDuracionMaxima;
-
-    @Column(name = "duracionMedia")
-	@NotEmpty
-	private Time parchisDuracionMedia;
-
-	@Column(name = "duracionTotal")
-	@NotEmpty
-	private Time parchisDuracionTotal;
-
-	@Column(name = "partidasJugadas")
-	@NotEmpty
-	private Integer ocaPartidasJugadas;
-
-	@Column(name = "promedioJugadores")
-	@NotEmpty
-	private Integer ocaPromedioJugadores;
-
-    @Column(name = "duracionMinima")
-	@NotEmpty
-	private Time ocaDuracionMinima;
-
-	@Column(name = "duracionMaxima")
-	@NotEmpty
-	private Time ocaDuracionMaxima;
-
-    @Column(name = "duracionMedia")
-	@NotEmpty
-	private Time ocaDuracionMedia;
-
-	@Column(name = "duracionTotal")
-	@NotEmpty
-	private Time ocaDuracionTotal;
+	private Estadisticas estadisticasGlobales;
 
 	@Column(name = "rankingFichasComidas")
 	@NotEmpty
