be6a3815bed6f7a3a63ee1ac44daec1befe7594c
Ignacio Planas <80254262+IgnacioPlanasTh@users.noreply.github.com>
2022-11-16 10:36:32 +0100

    Pull request develop<ignplathi
    Resolucion manual de conflictos

--- src/main/java/org/springframework/samples/petclinic/ocachis/usuario/UsuarioService.java
+++ src/main/java/org/springframework/samples/petclinic/ocachis/usuario/UsuarioService.java
@@ -1,19 +1,50 @@
 package org.springframework.samples.petclinic.ocachis.usuario;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import javax.validation.Valid;
 
-import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.ocachis.estadisticas.Estadisticas;
+import org.springframework.samples.petclinic.ocachis.user.AuthoritiesService;
+import org.springframework.samples.petclinic.ocachis.user.UserService;
+import org.springframework.stereotype.Service;
 
 @Service
-public class UsuarioService{
-    
-    private UsuarioRepository usuarioRepository;
-    
-    @Autowired
-	public UsuarioService(UsuarioRepository usuarioRepository){
+public class UsuarioService {
+	
+	private UsuarioRepository usuarioRepository;
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private AuthoritiesService authoritiesService;
+	
+	@Autowired
+	public UsuarioService(UsuarioRepository usuarioRepository) {
 		this.usuarioRepository = usuarioRepository;
+	}
+
+	@Transactional
+	public void saveUsuario(@Valid Usuario usuario) {
+		if(usuario.getEstadisticas()==null)usuario.setEstadisticas(new Estadisticas());
+		
+		// creamos el usuario
+		usuarioRepository.save(usuario);
+//		//creamos el user
+		userService.saveUser(usuario.getUser());
+//		//creamos authorities
+		authoritiesService.saveAuthorities(usuario.getUser().getUsername(), "player");
+		
+	}
+	
+	@Transactional
+	public void updateUsuario(@Valid Usuario usuario) {
+		usuarioRepository.updateUsuario(usuario.getId(), usuario.getNombre(), usuario.getApellido());
+		
+	}
+
+	public boolean existsUsuarioById(int usuarioId) {
+		return usuarioRepository.existsById(usuarioId);
 	}
     
     @Transactional(readOnly = true)
@@ -29,4 +60,5 @@ public class UsuarioService{
     public Usuario findUsuarioById(int id){
         return this.usuarioRepository.findById(id);
     }
+
 }
