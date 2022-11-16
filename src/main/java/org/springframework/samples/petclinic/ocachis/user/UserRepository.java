bf49c43fc9cd6bb76bc844268d029a3d2b3b3166
Ignacio Planas <80254262+IgnacioPlanasTh@users.noreply.github.com>
2022-11-16 10:30:02 +0100

    Pull request develop-ignplathi
    Resuleto conflicto en userRepository

--- src/main/java/org/springframework/samples/petclinic/ocachis/user/UserRepository.java
+++ src/main/java/org/springframework/samples/petclinic/ocachis/user/UserRepository.java
@@ -1,11 +1,8 @@
 package org.springframework.samples.petclinic.ocachis.user;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface UserRepository extends  CrudRepository<User, String>{
-	
-
-    
+	User findByUsername(String Username);
 }
